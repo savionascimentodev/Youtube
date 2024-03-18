2cfa2988a51b064c6f385fd876e91755c5a31763
Savio Nascimento <savionascimentodev@gmail.com>
2024-03-18 16:34:48 -0400

    ✨ add new components

--- shadcn-form/src/pages/_app.tsx
+++ shadcn-form/src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeToggle } from "@/components/ThemeToggle"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+      <ThemeToggle className="absolute top-6 right-6" />
+    </NextThemesProvider>
+  )
 }
